1718cc80cfc0c726177163601242b314caafff1a
Glitch (twitch-chatbot) <none>
2019-11-19 17:52:56 +0000

    💠🍮 Checkpoint ./bot.js:8702763/3585

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-const tmi = require('tmi.js');
+const tmi = require("tmi.js");
 
 // Define configuration options
 const opts = {
@@ -6,32 +6,71 @@ const opts = {
     username: process.env.BOT_USERNAME,
     password: process.env.OAUTH_TOKEN
   },
-  channels: [
-    process.env.CHANNEL_NAME
-  ]
+  channels: [process.env.CHANNEL_NAME]
 };
 
 // Create a client with our options
 const client = new tmi.client(opts);
 
 // Register our event handlers (defined below)
-client.on('message', onMessageHandler);
-client.on('connected', onConnectedHandler);
+client.on("message", onMessageHandler);
+client.on("connected", onConnectedHandler);
 
 // Connect to Twitch:
 client.connect();
 
 // Called every time a message comes in
-function onMessageHandler (target, context, msg, self) {
-  if (self) { return; } // Ignore messages from the bot
+function onMessageHandler(target, context, msg, self) {
+  if (self) {
+    return;
+  } // Ignore messages from the bot
 
   // Remove whitespace from chat message
   const commandName = msg.trim();
 
   // If the command is known, let's execute it
-  if (commandName === '!d20') {
+  if (commandName === "!d20") {
     const num = rollDice(commandName);
-    client.say(target, `You rolled a ${num}. Link: https://glitch.com/~twitch-chatbot`);
+    client.say(
+      target,
+      `You rolled a ${num}. Link: https://glitch.com/~twitch-chatbot`
+    );
+    console.log(`* Executed ${commandName} command`);
+  } else {
+    console.log(`* Unknown command ${commandName}`);
+  }
+
+
+  if (commandName === "!d20") {
+    const num = rollDice(commandName);
+    client.say(
+      target,
+      `You rolled a ${num}. Link: https://glitch.com/~twitch-chatbot`
+    );
+    console.log(`* Executed ${commandName} command`);
+  } else {
+    console.log(`* Unknown command ${commandName}`);
+  }
+
+
+  if (commandName === "!d20") {
+    const num = rollDice(commandName);
+    client.say(
+      target,
+      `You rolled a ${num}. Link: https://glitch.com/~twitch-chatbot`
+    );
+    console.log(`* Executed ${commandName} command`);
+  } else {
+    console.log(`* Unknown command ${commandName}`);
+  }
+
+
+  if (commandName === "!d20") {
+    const num = rollDice(commandName);
+    client.say(
+      target,
+      `You rolled a ${num}. Link: https://glitch.com/~twitch-chatbot`
+    );
     console.log(`* Executed ${commandName} command`);
   } else {
     console.log(`* Unknown command ${commandName}`);
@@ -39,12 +78,12 @@ function onMessageHandler (target, context, msg, self) {
 }
 
 // Function called when the "dice" command is issued
-function rollDice () {
+function rollDice() {
   const sides = 20;
   return Math.floor(Math.random() * sides) + 1;
 }
 
 // Called every time the bot connects to Twitch chat
-function onConnectedHandler (addr, port) {
+function onConnectedHandler(addr, port) {
   console.log(`* Connected to ${addr}:${port}`);
 }
